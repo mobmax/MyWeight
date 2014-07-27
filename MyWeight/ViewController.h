b7fbc4f3915035453f5e363466fef486f06792cb
Maxim Donchenko <maximd@maxim-mac-pro.local>
2014-07-27 16:45:11 +0400

    Initial Commit

---
+++ MyWeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWeight
+//
+//  Created by Maxim Donchenko on 27/07/14.
+//  Copyright (c) 2014 Maxim Donchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyWeight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyWeight
+//
+//  Created by Maxim Donchenko on 27/07/14.
+//  Copyright (c) 2014 Maxim Donchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
